18170d9c6135216eeff3851f763355da455e9c7e
Chris Baker <crystal@crystalsMacBook.hsd1.ca.comcast.net>
2019-06-26 12:57:48 -0700

    basically finished project
    this is basically the finished project

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 My bot for the discord hack week
 
 uses node.js as a base, with discord.js on top to allow me to interface with discord, and login as bot user. 
+
+in order to use the bot, you must first get a bot token from the developer page of disord, and put it into config.json, as well as set the command prefix to what you want e! by default.
+
+also needed to be configured is the amount of jokes and facts in the list, defaults are 79 facts and 73 jokes. the facts and jokes are stored in jokes.json inside a array for each one.
+
+PLEASE KEEP YOUR BOT TOKEN PRIVATE
+
+if this bot is edited or altered in any way, please open source the bot and give credits to me where due.
+
+simplified commands to fact, joke, about me, and about bot only
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+//importing stuff, like the start a;ways is
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix, token , numOfFacts, numOfJokes} = require('../config.json');
 const client = new Discord.Client();
-const { facts, arms, lame, me, } = require('./jokes.json');
+const { facts, arms, me } = require('./jokes.json');
 
 //code ran when bot initiated
 client.once('ready', () => {
     console.log('Ready!');
 })
 
-//random number generator, pass total in to tell the generator how many jokes are in the list
+//random number generator, pass total in to tell the generator how many jokes are in the list, simple but effective!
 function random(total){
     return Math.floor(Math.random()*total);
 }
@@ -18,10 +19,9 @@ function random(total){
 client.on('message', message => {
             //message.channel.send("Me"); <<< you shouldn't see this, what are you doing here?
             //the actual responces, where it checks what exact joke bank to pull from
-            if(message.content.startsWith(`${prefix}joke useless facts`)){message.channel.send(facts[random(10)]);}
-            if(message.content.startsWith(`${prefix}joke arms`)){message.channel.send("What do you call "+arms[random(31)]);}
-            if(message.content.startsWith(`${prefix}help`)){message.channel.send("Welcome to eevee-bot!\nthe following commands are currently working:\ne!joke useless fact : gives a useless fact\ne!joke lame : lame joke. self explainatory\ne!about author : about the author!\ne!about bot : about me\ne!help : this page\nand another secret command or two. guess if you read the source code, you'd know.");}
-            if(message.content.startsWith(`${prefix}joke lame`)){message.channel.send(lame[random(31)]);}
+            if(message.content.startsWith(`${prefix}fact`)){message.channel.send(facts[random(numOfFacts)]);}
+            if(message.content.startsWith(`${prefix}joke`)){message.channel.send(arms[random(numOfJokes)]);}
+            if(message.content.startsWith(`${prefix}help`)){message.channel.send("Welcome to eevee-bot!\nthe following commands are currently working:\ne!fact : gives a useless fact (there are "+numOfFacts+" facts)\ne!joke : lame joke. self explainatory (there are "+numOfJokes+" jokes)\ne!about author : about the author!\ne!about bot : about me\ne!help : this page.");}
             if(message.content.startsWith(`${prefix}about me`)){message.channel.send(me[random(100)]);}
             if(message.content.startsWith(`${prefix}about bot`)){message.channel.send("This is a bot that was made for the first yearly discord hack! maybe I'll work on one next year as well, but we'll see when that comes around! for now, this is just a starting point in programming for me, pretty much the first thing I have ever made myself. I'm kinda new to programming.");}
             if(message.content.startsWith(`EeveeBot`)){message.channel.send("Thanks for the mention!");}
