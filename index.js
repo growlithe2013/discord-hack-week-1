32da4a499604652b2c803df5a6a1a11b5a2adaeb
Chris Baker <crystal@crystals-MacBook.local>
2019-07-09 13:33:45 -0700

    Update index.js
    fixed the bot, because I'm a retard and broke it

--- index.js
+++ index.js
@@ -18,6 +18,19 @@ function random(total){
 //code to read messages, and respond in the right chat with the joke.
 //when message recieved, check for the prefix in config.json
 client.on('message', message => {
+
+    //borrowed script to get username of poster, used for the mini RP
+   /* user = message.member;
+    user = user.toString();
+    if (user.includes("!")) {
+        user = user.split("!")[1].split(">")[0];
+    } else {
+        user = user.split("@")[1].split(">")[0];
+    }
+    username = client.users.get(user).username
+
+    world_dict[username];*/
+
             //message.channel.send("Me"); <<< you shouldn't see this, what are you doing here?
             //the actual responces, where it checks what exact joke bank to pull from
             if(message.content.startsWith(`${prefix}fact`)){message.channel.send(facts[random(numOfFacts)]);}
@@ -29,7 +42,35 @@ client.on('message', message => {
             if(message.content.startsWith(`eeveeBot`)){message.channel.send("Thanks for the mention!");}
             if(message.content.startsWith(`Eeveebot`)){message.channel.send("Thanks for the mention!");}
             if(message.content.startsWith(`eeveebot`)){message.channel.send("Thanks for the mention!");}
-            if(message.content.statsWith(`${prefix}number`)){message.channel.send(Math.floor(Math.random()*10));}
+            if(message.content.startsWith(`${prefix}number`)){message.channel.send(Math.ceil(Math.random()*10));   }
+            
+            /* if(message.content.startsWith(`${prefix}rp`)){
+                //ha. e rp.. unintentional dirty joke.
+                if(world_dict[username]==null || world_dict[username]==0){
+                    world_dict[username] = 1;
+                    message.channel.send("You wake up in a cabin in the woods, and look around you. It's snowing lightly out. You realize there's no food in the cabin, and that you need to go find some food, so you put on your jacket and step outside. To your left is an open field, in front of you is a path to a river, which you can here rushing along, and to your right is a thick forrest. what direction do you decide to go?");
+                    
+                }else if(world_dict[username]==1){
+                    world_dict[username] = 2;
+                    message.channel.send("You look to your left briefly, then to your right. You realize that with the snow, there won't be any real prey anywhere, and decide to go to the river to try to catch some fish.");
+                    message.channel.send("You soon reach the river, and look in. you see some fish swimming around. Do you want to jump in here and try to catch them by hand, or look around to try to make a fishing pole?");
+                    
+                }else if(world_dict[username]==2){
+                    world_dict[username] = 3;
+                    message.channel.send("You start to look around, but soon after your hunger gets the best of you, and you jump right in to try to catch the fish, most of them swimming a bit away, but a few staying. Do you want to dive to catch one, or wait for them to come back?");
+                    
+                }else if(world_dict[username]==3){
+                    world_dict[username] = 4;
+                    message.channel.send("you pause for a moment, and think, but just a moment before your hunger gets the best of you again, and you dive down to try and catch some fish, but you end up catching a rock with your foot and start to drown. do you want to try to swim ashore, or acccept your fate?");
+                    
+                }else if(world_dict[username] ==4){
+                    world_dict[username] = 0;
+                    message.channel.send("You try to swim back to shore, before starting to lose hope. you do, however, manage to make it, laying there for a bit. Your vision slowly fades out to back, and before you know it, you start to come back to\n..\n..\n..\n..\n..");
+                    message.channel.send("you look around you; You are sitting in a cart, bound. The person across from you starts to talk. He says \"Hey, you. You're finally awake. You were trying to cross the border, right? Walked right into that Imperial ambush, same as us, and that thief over there.\"")
+                    
+                }
+            
+            }*/
             }
             //if(message.content.startsWith(`@EeveeBot`)){message.channel.send("Thanks for the mention!");}   
     
